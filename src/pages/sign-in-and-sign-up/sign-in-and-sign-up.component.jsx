6de0ec0ee7726229650f643956936bb2bd49ce61
Kartikey-Dwivedi <63398278+Kartikey-Dwivedi@users.noreply.github.com>
2020-04-28 03:01:54 +0530

    implemented firebase utils, include ability to store authenticated users into our firestore databae

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/Sign-in/sign-in.component';
+
+import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignup =() => (
     <div className='sign-in-and-sign-up'>
        < SignIn />
+       < SignUp />
     </div>
 );
 
